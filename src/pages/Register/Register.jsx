ceb3b2b23ea8fc913836c49dcaa8dfa5ae3dc2b2
mehedi hasan <mhshohan04@gmail.com>
2025-10-23 09:43:44 +0600

    password toggle show

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,15 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react'
 import auth from '../../firebase/firebase.config';
+import { BsEye } from 'react-icons/bs';
+import { BsEyeSlash } from 'react-icons/bs';
+import { FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
 
   const handleRegister = (e) => {
     e.preventDefault();
@@ -40,6 +45,11 @@ const Register = () => {
      })
   }
 
+  const handleTogglePasswordShow = e => {
+    e.preventDefault();
+    setShowPassword(!showPassword);
+  }
+
   return (
     <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -54,7 +64,18 @@ const Register = () => {
           <label className="label">Email</label>
           <input type="email" name='email' className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" name='password' className="input" placeholder="Password" />
+
+          <div className='relative'>
+            <input type={showPassword ? 'text ': 'password'}
+               name='password'
+               className="input" placeholder="Password" />
+            <button
+               onClick={handleTogglePasswordShow}
+               className='btn btn-xs top-2 right-6 absolute'>
+                { showPassword ? <BsEyeSlash></BsEyeSlash> : <BsEye></BsEye>}
+               </button>
+          </div>
+
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-info mt-4">Login</button>
         </fieldset>
