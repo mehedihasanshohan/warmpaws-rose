b3309b6af6f9d186879d83f3202cd375415ada92
mehedi hasan <mhshohan04@gmail.com>
2025-10-25 12:43:54 +0600

    added icon in footer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { GoogleAuthProvider } from 'firebase/auth'
 import React, { use} from 'react'
-import { NavLink} from 'react-router'
+import { Link, NavLink} from 'react-router'
 import { AuthContext } from '../../context/AuthContext.';
 import { toast } from 'react-toastify';
 
@@ -23,19 +23,20 @@ const Navbar = () => {
    <div className="navbar bg-base-100 shadow-sm  bg-linear-to-r from-teal-600 via-teal-700 to-teal-600">
   <div className="navbar-start">
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      <div tabIndex={0} role="button" className="btn btn-info lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
       <ul
         tabIndex="-1"
         className="menu menu-md dropdown-content bg-base-400 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><NavLink>Home</NavLink></li>
-        <li><NavLink>Services</NavLink></li>
-        <li><NavLink>My Profile</NavLink></li>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/services'>Services</NavLink></li>
+        <li><NavLink to='/my-profile'>My Profile</NavLink></li>
       </ul>
     </div>
-    <a className="text-3xl font-semibold text-white ml-2 animate__animated animate__rollIn">
-      <span className='text-amber-400 text-3xl font-semibold'>warmpaws</span> rose</a>
+    <Link to='/' className="text-3xl font-semibold text-white ml-2 animate__animated animate__rollIn">
+      <span className='text-amber-400 text-3xl font-semibold'>warmpaws rose</span>
+    </Link>
   </div>
   <div className="navbar-center hidden lg:flex animate__animated animate__fadeInLeft" >
     <ul className="flex justify-center items-center gap-4 text-white  font-semibold text-2xl px-1">
